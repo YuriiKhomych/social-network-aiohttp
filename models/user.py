67b5692a9064fea0c43a6171fffd3748bcdf5157
yuriikhomych <yuriykhomich@gmail.com>
2018-09-22 09:28:30 +0300

    Add create_new_user method for User model

--- models/user.py
+++ models/user.py
@@ -1,3 +1,7 @@
+import hashlib
+from motor.motor_asyncio import AsyncIOMotorDatabase
+
+
 class User:
 
     collection = None
@@ -8,3 +12,20 @@ class User:
     @staticmethod
     async def get_user(db, uid):
         return await db.users.find_one(uid)
+
+    @staticmethod
+    async def create_new_user(db: AsyncIOMotorDatabase, data):
+        email = data['email']
+        user = await db.users.find_one({'email': email})
+        if user:
+            return dict(error='user with email {} exist'.format(email))
+
+        if data['first_name'] and data['last_name'] and data['password']:
+            data = dict(data)
+            data['password'] = hashlib.sha256(
+                data['password'].encode('utf8')).hexdigest()
+            result = await db.users.insert_one(data)
+            return result
+        else:
+            return dict(error='Missing user data parameters')
+
