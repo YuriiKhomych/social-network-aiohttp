383383f49b665acd3f31e33a14a20f128d405592
yuriikhomych <yuriykhomich@gmail.com>
2018-09-22 13:04:11 +0300

    Add static_dir to config

--- config/common.py
+++ config/common.py
@@ -4,3 +4,4 @@ class BaseConfig:
     app_name = 'Social Network'
     secret_key = b'TyzLMReLCWUiPsTFMActw_0dtEU7kAcFXHNYYm64DNI='
     database_name = 'my_database'
+    static_dir = '/Users/yuriikhomych/PycharmProjects/social-network-aiohttp/static'
